1e74edcb27f0099cc58da4f6d890b53bd11b66cf
ivantoskov <ivantoskov17@gmail.com>
2020-12-09 22:14:58 +0200

    Initial Commit

---
+++ turing-machine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  turing-machine
+//
+//  Created by Ivan Toskov on 09/12/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
